e6183f2b85cc85123d11541b921350a464b5c52f
Tomas Dacik <xdacik00@stud.fit.vutbr.cz>
2022-07-01 19:15:46 +0200

    Rework bound computation using SL graphs

---
+++ src/bounds/SL_graph.ml
@@ -0,0 +1,173 @@
+(* SL graphs
+ *
+ * Author: Tomas Dacik (xdacik00@fit.vutbr.cz), 2022 *)
+
+open Graph
+
+module SL_edge = struct
+
+  type t =
+    | No
+    | Pointer
+    | List
+    | Equality
+    | Disequality
+    [@@deriving compare, equal]
+
+  let default = No
+
+  let length = function
+    | Pointer -> 1
+    | List -> 2
+    | _ -> failwith "length undefined"
+
+  let show = function
+    | Pointer     -> "->"
+    | List        -> "~>"
+    | Equality    -> "="
+    | Disequality -> "!="
+
+  module Self = struct
+    type nonrec t = t
+    let compare = compare
+    let show = show
+  end
+
+  include Datatype.Printable(Self)
+  include Datatype.Collections(Self)
+
+end
+
+open SL_edge
+
+module G = struct
+
+  module Self = Graph.Persistent.Digraph.ConcreteBidirectionalLabeled
+    (SSL.Variable)
+    (SL_edge)
+
+  include Self
+  include Graph.Oper.P(Self)
+
+  include Path.Dijkstra
+    (Self)
+    (struct
+      type edge = Self.E.t
+      type t = Int.t
+      let weight e = SL_edge.length @@ Self.E.label e
+      let compare = Int.compare
+      let add = (+)
+      let zero = 0
+    end)
+
+  include Graph.Graphviz.Dot
+    (struct
+      include Self
+
+      let graph_attributes _ = []
+      let default_vertex_attributes _ = []
+      let vertex_name = SSL.Variable.show
+      let vertex_attributes v = []
+
+      let get_subgraph _ = None
+      let edge_attributes e = [
+        `Label (SL_edge.show @@ E.label e);
+        `Arrowhead (match E.label e with | Pointer | List -> `Normal | _ -> `None);
+        `Style (match E.label e with | Pointer | List -> `Solid | _ -> `Dashed)
+      ]
+      let default_edge_attributes _ = []
+    end)
+
+end
+
+let projection g labels =
+  G.fold_edges_e
+    (fun (x, label, y) g ->
+      if List.mem label labels then G.add_edge_e g (x, label, y)
+      else g
+    ) g G.empty
+
+let projection_eq g = projection g [Equality]
+let projection_neq g = projection g [Disequality]
+let projection_pointer g = projection g [Pointer]
+let spatial_projection g = projection g [Pointer; List]
+
+(* TODO: transitivity *)
+let must_eq g x y =
+  let g = projection_eq g in
+  G.mem_edge g x y || G.mem_edge g y x
+
+let must_neq g x y =
+  let g = projection_neq g in
+  G.mem_edge g x y || G.mem_edge g y x
+
+let must_pointer g x y =
+  let g = projection_pointer g in
+  G.mem_edge g x y
+
+let nb_must_pointers g =
+  let g = projection_pointer g in
+  let vertices = G.fold_edges (fun x y acc -> x :: acc) g [] in
+  List.length vertices
+
+let nb_allocated g =
+  let g = projection_pointer g in
+  let vertices = G.fold_edges (fun x y acc -> x :: acc) g [] in
+  List.length vertices
+
+let must_path g x y max =
+  Printf.printf "max: %d\n" max;
+  let g = projection_pointer g in
+  let channel = open_out "a.dot" in
+  G.output_graph channel g;
+  close_out channel;
+  try
+    let _, weight = G.shortest_path g x y in
+    weight
+  with _ -> max
+
+(* Precondition : g is already a pointer projection *)
+let nb_must_preds_v g x =
+  G.fold_vertex List.cons g []
+  |> List.filter (fun v -> G.mem_edge g v x)
+  |> List.length
+  |> (fun x -> max (x-1) 0)
+
+let nb_must_forks g =
+  let g = projection_pointer g in
+  G.fold_vertex List.cons g []
+  |> List.map (fun v -> nb_must_preds_v g v)
+  |> BatList.sum
+
+let predict_footprint g x y =
+  let g = spatial_projection g in
+  let path, _ = G.shortest_path g x y in
+  List.fold_left
+    (fun (ptrs, lists) (x', label, y') ->
+      match label with
+      | Pointer -> ((SSL.PointsTo (x', y')) :: ptrs, lists)
+      | List -> (ptrs, (SSL.LS (x', y')) :: lists)
+    ) ([], []) path
+
+let rec compute phi = match phi with
+  | SSL.Eq (x, y) -> G.add_edge_e G.empty (x, Equality, y)
+  | SSL.Neq (x, y) -> G.add_edge_e G.empty (x, Disequality, y)
+  | SSL.PointsTo (x, y) -> G.add_edge_e G.empty (x, Pointer, y)
+  | SSL.LS (x, y) -> G.add_edge_e G.empty (x, List, y)
+
+  | SSL.Star (psi1, psi2) -> G.union (compute psi1) (compute psi2)
+  | SSL.And (psi1, psi2) -> G.union (compute psi1) (compute psi2)
+  | SSL.Or (psi1, psi2) -> G.intersect (compute psi1) (compute psi2)
+  | SSL.GuardedNeg (psi1, psi2) -> compute psi1
+
+  | SSL.Septraction _ -> G.empty
+  | SSL.Not _ -> G.empty
+
+let compute phi = compute phi
+
+let output_file g path =
+  let channel = open_out path in
+  G.output_graph channel g;
+  close_out channel
+
+include G
--- src/solver/debug.ml
+++ src/solver/debug.ml
@@ -13,14 +13,15 @@ let path_ast file = debug_dir ^ "/" ^ file ^ ".dot"
 let path_smt_model = debug_dir ^ "/" ^ "smt_model.out"
 let path_model_dot = debug_dir ^ "/" ^ "model.dot"
 
-let length_graph_dot = debug_dir ^ "/" ^ "length_graph.dot"
-let length_proj_dot = debug_dir ^ "/" ^ "pointer_graph.dot"
+let sl_graph_dot = debug_dir ^ "/" ^ "length_graph.dot"
 
 let debug_out file content =
   let path = debug_dir ^ "/" ^ file in
   let channel = open_out path in
   Printf.fprintf channel "%s" content;
   close_out channel
+
+let query_counter = ref 0
 
 (** Recursively remove a directory *)
 let rec rm path =
@@ -55,30 +56,23 @@ let formula suffix phi =
   SSLUtils.out_ast (path_ast out_file) phi
 
 let context context =
-  LengthGraph.output_file context.length_graph length_graph_dot;
-  LengthGraph.output_file (LengthGraph.pointer_projection context.length_graph) length_proj_dot
+  SL_graph.output_file context.sl_graph sl_graph_dot
 
-let translated (context, phi) =
-  debug_out "translated.smt2" (Z3.Expr.to_string phi);
-  debug_out "unsimplfied_translated.smt2" (Z3.Expr.to_string phi);
-  debug_out "z3_input.smt2" (Z3.SMT.benchmark_to_smtstring context
-    "Input for solver"
-    "ALL"
-    "unknown"
-    ""
-    []
-    phi
-  )
+let qf_phi (context, phi) =
+  debug_out "phi_base.smt2" (Z3.Expr.to_string phi)
+
+let translated str = debug_out "translated.smt" str
 
 let model sh =
   debug_out "sh.out" (StackHeapModel.to_string sh);
   StackHeapModel.output_graph path_model_dot sh
 
 let smt_model model =
-  debug_out "smt_model.smt2" (Z3.Model.to_string model)
+  debug_out "smt_model.out" (SMT.Model.show model)
 
 (** Decorated functions *)
 let context    = decorate context
+let qf_phi     = decorate qf_phi
 let translated = decorate translated
 let model      = decorate model
 let smt_model  = decorate smt_model
