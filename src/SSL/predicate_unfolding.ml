4c1be90f3840923f2822f4cc69150d638a6f317a
Tomas Dacik <xdacik00@stud.fit.vutbr.cz>
2023-02-08 20:34:03 +0100

    Refacor predicate unfolding

--- src/SSL/predicate_unfolding.ml
+++ src/SSL/predicate_unfolding.ml
@@ -3,24 +3,22 @@
  * Author: Tomas Dacik (xdacik00@fit.vutbr.cz), 2022 *)
 
 open SSL
-open Batteries
 
-let name = "predicate unfolding"
-
-(** Unfold ls(x,y) n times *)
-let rec unfold_ls_n n x y = match n with
-  | 0 -> Eq (x, y)
+(** Unfold list segment predicates n times *)
+let rec unfold_ls_n x y n = match n with
+  | 0 -> SSL.mk_eq x y
+  | 1 -> SSL.mk_star [SSL.mk_distinct x y; SSL.mk_pto x y]
   | n ->
-      let l = Var (SSL.Variable.mk_fresh "l") in
-      Star (Neq (x,y), Star (PointsTo (x, [l]), unfold_ls_n (n-1) l y))
+      let l = SSL.mk_fresh_var "l" in
+      Star (SSL.mk_distinct x y, Star (PointsTo (x, [l]), unfold_ls_n l y (n-1)))
 
-let unfold_ls n x y =
+(** Create disjunction of all unfolding up to length n *)
+let unfold_ls x y n =
   BatList.range 0 `To n
-  |> BatList.map (fun n -> unfold_ls_n n x y)
+  |> List.map (unfold_ls_n x y)
   |> SSL.mk_or
 
 (** Recursively apply unfolding on all subformulae *)
-(** TODO: use some kind of fold/map? *)
 let rec unfold phi n = match phi with
   | And (f1, f2) -> And (unfold f1 n, unfold f2 n)
   | Or (f1, f2) -> Or (unfold f1 n, unfold f2 n)
@@ -28,9 +26,5 @@ let rec unfold phi n = match phi with
   | GuardedNeg (f1, f2) -> GuardedNeg (unfold f1 n, unfold f2 n)
   | Star (f1, f2) -> Star (unfold f1 n, unfold f2 n)
   | Septraction (f1, f2) -> Septraction (unfold f1 n, unfold f2 n)
-  | LS (x, y) -> unfold_ls n x y
+  | LS (x, y) -> unfold_ls x y n
   | atom -> atom
-
-let convert phi n = Smtlib_convertor.translate_all (unfold phi n)
-
-let dump file phi status n = Smtlib_convertor.dump file (unfold phi n) status
--- src/SSL/predicate_unfolding.mli
+++ src/SSL/predicate_unfolding.mli
@@ -2,11 +2,5 @@
  *
  * Author: Tomas Dacik (xdacik00@fit.vutbr.cz), 2022 *)
 
-val name : string
-
 val unfold : SSL.t -> int -> SSL.t
 (** Bounded unfolding of list-segment predicates *)
-
-val convert : SSL.t -> int -> string
-
-val dump : string -> SSL.t -> string -> int -> unit
