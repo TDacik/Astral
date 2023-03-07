0d2124d582dbb24fb5f368c8a2647db57122f045
Tomas Dacik <xdacik00@stud.fit.vutbr.cz>
2023-03-07 01:17:45 +0100

    [SSL] Better visualisation of formulas' AST using rich syntax of SSL

--- src/SSL/RichSyntax.ml
+++ src/SSL/RichSyntax.ml
@@ -3,77 +3,118 @@
  * This module is intended for pretty-printing of formulae. All computations should
  * be performed with formulas in minimal syntax as defined in module SSL.
  *
- * Author: Tomas Dacik (xdacik00@fit.vutbr.cz), 2022
+ * Author: Tomas Dacik (xdacik00@fit.vutbr.cz), 2022 *)
+
+open Logic_sig
 
 module Variable = SSL.Variable
 
 type t =
+  | Var of SSL.Variable.t     (* Location variable *)
+  | Pure of SMT.Term.t        (* Pure boolean term which does not contain location variables *)
+
+  (* Atoms *)
+  | Eq of t list
+  | Distinct of t list
+  | PointsTo of t * t list
+  | LS of t * t
+  | DLS of t * t * t * t
+  | SkipList of int * t * t
+
+  (* Boolean connectives *)
   | And of t * t
   | Or of t * t
   | Not of t
   | GuardedNeg of t * t
-  | Star of t * t
-  | Septraction of t * t
-  | LS of Variable.t * Variable.t
-  | PointsTo of Variable.t * Variable.t
-  | Eq of Variable.t * Variable.t
-  | Neq of Variable.t * Variable.t
 
+  (* Quantifiers *)
+  | Exists of t list * t
+  | Forall of t list * t
+
+  (* Spatial connectives *)
+  | Star of t list
+  | Septraction of t * t
+
+  (* Syntactic sugar *)
   | True
   | False
   | Emp
-  | Wand of t * t
-[@@deriving compare, equal]
+  | Implies of t * t
+  | Iff of t * t
 
-type arity =
-  | Atom'
-  | Atom of Variable.t * Variable.t
-  | Unary of t
-  | Binary of t * t
+let rec of_ssl phi = match phi with
+  (* Syntactic sugar *)
+  | phi when SSL.is_true phi -> True
+  | phi when SSL.is_false phi -> False
+  | phi when SSL.is_emp phi -> Emp
+  | (SSL.Or (SSL.Not lhs, rhs)) -> Implies (of_ssl lhs, of_ssl rhs)
+  | (SSL.Or (rhs, SSL.Not lhs)) -> Implies (of_ssl lhs, of_ssl rhs)
+  | SSL.And (lhs, rhs) ->
+    begin try
+      let lhs', rhs' = SSL.get_iff_operands phi in
+      Iff (of_ssl lhs', of_ssl rhs')
+    with _ -> And (of_ssl lhs, of_ssl rhs)
+    end
 
-let get_arity = function
-  | And (f1, f2) -> Binary (f1, f2)
-  | Or (f1, f2) -> Binary (f1, f2)
-  | Not f -> Unary f
-  | GuardedNeg (f1, f2) -> Binary (f1, f2)
-  | Star (f1, f2) -> Binary (f1, f2)
-  | Septraction (f1, f2) -> Binary (f1, f2)
-  | LS (v1, v2) -> Atom (v1, v2)
-  | PointsTo (v1, v2) -> Atom (v1, v2)
-  | Eq (v1, v2) -> Atom (v1, v2)
-  | Neq (v1, v2) -> Atom (v1, v2)
+  (* Other formulae *)
+  | SSL.Var v -> Var v
+  | SSL.Pure t -> Pure t
+  | SSL.Eq xs -> Eq (List.map of_ssl xs)
+  | SSL.Distinct xs -> Distinct (List.map of_ssl xs)
+  | SSL.PointsTo (x, ys) -> PointsTo (of_ssl x, List.map of_ssl ys)
+  | SSL.LS (x, y) -> LS (of_ssl x, of_ssl y)
+  | SSL.DLS (x, y, f, l) -> DLS (of_ssl x, of_ssl y, of_ssl f, of_ssl l)
+  | SSL.SkipList (depth, x, y) -> SkipList (depth, of_ssl x, of_ssl y)
+  | SSL.Or (psi1, psi2) -> Or (of_ssl psi1, of_ssl psi2)
+  | SSL.Not psi -> Not (of_ssl psi)
+  | SSL.GuardedNeg (psi1, psi2) -> GuardedNeg (of_ssl psi1, of_ssl psi2)
+  | SSL.Exists (xs, psi) -> Exists (List.map of_ssl xs, of_ssl psi)
+  | SSL.Forall (xs, psi) -> Forall (List.map of_ssl xs, of_ssl psi)
+  | SSL.Star psis -> Star (List.map of_ssl psis)
+  | SSL.Septraction (psi1, psi2) -> Septraction (of_ssl psi1, of_ssl psi2)
 
-  | True -> Atom'
-  | False -> Atom'
-  | Emp -> Atom'
-  | Wand (f1, f2) -> Binary (f1, f2)
+and to_ssl = function
+  | Var v -> SSL.Var v
+  | Pure t -> SSL.Pure t
+  | Eq xs -> SSL.Eq (List.map to_ssl xs)
+  | Distinct xs -> SSL.Distinct (List.map to_ssl xs)
+  | PointsTo (x, ys) -> SSL.PointsTo (to_ssl x, List.map to_ssl ys)
+  | LS (x, y) -> SSL.LS (to_ssl x, to_ssl y)
+  | DLS (x, y, f, l) -> SSL.DLS (to_ssl x, to_ssl y, to_ssl f, to_ssl l)
+  | SkipList (depth, x, y) -> SSL.SkipList (depth, to_ssl x, to_ssl y)
+  | And (psi1, psi2) -> SSL.And (to_ssl psi1, to_ssl psi2)
+  | Or (psi1, psi2) -> SSL.Or (to_ssl psi1, to_ssl psi2)
+  | Not psi -> SSL.Not (to_ssl psi)
+  | GuardedNeg (psi1, psi2) -> SSL.GuardedNeg (to_ssl psi1, to_ssl psi2)
+  | Exists (xs, psi) -> SSL.Exists (List.map to_ssl xs, to_ssl psi)
+  | Forall (xs, psi) -> SSL.Forall (List.map to_ssl xs, to_ssl psi)
+  | Star psis -> SSL.Star (List.map to_ssl psis)
+  | Septraction (psi1, psi2) -> SSL.Septraction (to_ssl psi1, to_ssl psi2)
 
-let is_emp = function
-  | SSL.Eq (Variable.nil, Variable.nil) -> true
-  | _ -> false
+  | True -> SSL.mk_true ()
+  | False -> SSL.mk_false ()
+  | Emp -> SSL.mk_emp ()
+  | Implies (lhs, rhs) -> SSL.mk_implies (to_ssl lhs) (to_ssl rhs)
+  | Iff (lhs, rhs) -> SSL.mk_iff [to_ssl lhs; to_ssl rhs]
 
-let is_true = function
-  | SSL.Or (SSL.Not psi1, psi2) -> is_emp psi1 && is_emp psi2
-  | _ -> false
+let description_of_ssl phi = match SSL.describe_node @@ to_ssl phi with
+  | name, Logic_sig.Var (x, sort) -> name, Logic_sig.Var (x, sort)
+  | name, Operator (xs, sort) -> name, Operator (List.map of_ssl xs, sort)
+  | name, Connective xs -> name, Connective (List.map of_ssl xs)
+  | name, Quantifier (xs, psi) -> name, Quantifier (List.map of_ssl xs, of_ssl psi)
 
-let is_false = function
-  | SSL.And (SSL.Not psi1, psi2) -> is_emp psi1 && is_emp psi2
-  | SSL.Not psi -> is_true psi
+let describe_node = function
+  (* Syntactic sugar *)
+  | True -> ("true", Connective [])
+  | False -> ("false", Connective [])
+  | Emp -> ("emp", Connective [])
+  | Implies (lhs, rhs) -> ("=>", Connective [lhs; rhs])
+  | Iff (lhs, rhs) -> ("<=>", Connective [lhs; rhs])
+  | other -> description_of_ssl other
 
-let rec sugarize phi =
-  if is_emp phi then Emp
-  else if is_true phi then True
-  else if is_false phi then False
-  else match phi with
-    | SSL.And (f1, f2) -> And (sugarize f1, sugarize f2)
-    | SSL.Or (f1, f2) -> Or (sugarize f1, sugarize f2)
-    | SSL.Not f -> Not (sugarize f)
-    | SSL.GuardedNeg (f1, f2) -> GuardedNeg (sugarize f1, sugarize f2)
-    | SSL.Star (f1, f2) -> Star (sugarize f1, sugarize f2)
-    | SSL.Septraction (f1, f2) -> Septraction (sugarize f1, sugarize f2)
-    | SSL.LS (v1, v2) -> LS (v1, v2)
-    | SSL.PointsTo (v1, v2) -> PointsTo (v1, v2)
-    | SSL.Eq (v1, v2) -> Eq (v1, v2)
-    | SSL.Neq (v1, v2) -> Neq (v1, v2)
+module Self = struct
+  type nonrec t = t
+  let describe_node = describe_node
+end
 
-*)
+include Logic.Make(Self)
--- src/SSL/SSLUtils.ml
+++ src/SSL/SSLUtils.ml
@@ -2,7 +2,7 @@
  *
  * Author: Tomas Dacik (xdacik00@fit.vutbr.cz), 2022 *)
 
-open SSL
+open RichSyntax
 
 module G = struct
 
@@ -12,7 +12,7 @@ module G = struct
     let hash = Hashtbl.hash
   end)
   (struct
-    type t = [`Left | `Right]
+    type t = [`Left | `Right | `None]
     let compare = Stdlib.compare
     let default = `Left
   end)
@@ -27,39 +27,38 @@ module AST = struct
   type t = G.t * G.V.t
 
   let show_seq = function
-    | [x] -> SSL.show x
-    | xs -> "<" ^ (List.map SSL.show xs |> String.concat ", ") ^ ">"
+    | [x] -> show x
+    | xs -> "<" ^ (List.map show xs |> String.concat ", ") ^ ">"
 
-  let is_syntactic_sugar phi = is_true phi || is_false phi || is_emp phi
-
-  let node_name phi psi =
-    let label =
-      (* Syntactic sugar *)
-      if is_true psi then "true"
-      else if is_false psi then "false"
-      else if is_emp psi then "emp"
-      else match psi with
+  let node_name top phi psi =
+    let label = match RichSyntax.of_ssl psi with
+      | True -> "true"
+      | False -> "false"
+      | Emp -> "emp"
+      | Implies (_, _) -> "→"
+      | Iff (_, _) -> "↔"
       | Var _ | Pure _ -> SSL.show psi
       | And (f1, f2) -> "∧"
       | Or (f1, f2) -> "∨"
       | Not f -> "¬"
+      | GuardedNeg (f1, f2) when top -> "⊨"
       | GuardedNeg (f1, f2) -> "∧¬"
-      | Star (f1, f2) -> "★"
+      | Star _ -> "★"
       | Septraction (f1, f2) -> "--(★)"
-      | LS (v1, v2) -> Format.asprintf "ls(%a, %a)" SSL.pp v1 SSL.pp v2
+      | LS (v1, v2) -> Format.asprintf "ls(%a, %a)" pp v1 pp v2
       | DLS (v1, v2, v3, v4) -> Format.asprintf "dls(%a, %a, %a, %a)"
-                                  SSL.pp v1 SSL.pp v2 SSL.pp v3 SSL.pp v4
-      | PointsTo (v1, vs) -> Format.asprintf "%a ↦ %s" SSL.pp v1 (show_seq vs)
+                                  pp v1 pp v2 pp v3 pp v4
+      | PointsTo (v1, vs) -> Format.asprintf "%a ↦ %s" pp v1 (show_seq vs)
 
-      | Eq [x; y] -> Format.asprintf "%a = %a" SSL.pp x SSL.pp y
-      | Eq xs -> Format.asprintf "equals(%s)" (String.concat ", " @@ List.map SSL.show xs)
+      | Eq [x; y] -> Format.asprintf "%a = %a" pp x pp y
+      | Eq xs -> Format.asprintf "equals(%s)" (String.concat ", " @@ List.map show xs)
 
-      | Distinct [x; y] -> Format.asprintf "%a ≠ %a" SSL.pp x SSL.pp y
+      | Distinct [x; y] -> Format.asprintf "%a ≠ %a" pp x pp y
       | Distinct xs ->
-          Format.asprintf "distinct(%s)" (String.concat ", " @@ List.map SSL.show xs)
+          Format.asprintf "distinct(%s)" (String.concat ", " @@ List.map show xs)
 
-      | Exists (xs, _) -> Format.asprintf "∃ %s" (String.concat ", " @@ List.map SSL.show xs)
-      | Forall (xs, _) -> Format.asprintf "∀ %s" (String.concat ", " @@ List.map SSL.show xs)
+      | Exists (xs, _) -> Format.asprintf "∃ %s" (String.concat ", " @@ List.map show xs)
+      | Forall (xs, _) -> Format.asprintf "∀ %s" (String.concat ", " @@ List.map show xs)
 
     in
     Format.asprintf "\"%d: %s\"" (SSL.subformula_id phi psi) label
@@ -78,30 +77,53 @@ module AST = struct
     let tree = G.add_edge_e tree (v, `Right, rhs_root) in
     (tree, v)
 
-  let rec create phi psi =
-    if is_syntactic_sugar psi then
-      make_leaf (node_name phi psi)
+  let mk_variadic_node trees v =
+    let tree0 = G.add_vertex G.empty v in
+    let tree =
+      List.fold_left
+        (fun acc (tree, root) ->
+          let g = G.union tree acc in
+          G.add_edge_e g (v, `None, root)
+        ) tree0 trees
+    in
+    (tree, v)
+
+  let rec create ?(top=false) phi psi =
+    if SSL.is_true psi then make_leaf "true"
+    else if SSL.is_false psi then make_leaf "false"
+    else if SSL.is_emp psi then make_leaf "emp"
+    else if SSL.is_implies psi then
+      let lhs, rhs = SSL.get_implies_operands psi in
+        let lhs = create phi lhs in
+        let rhs = create phi rhs in
+        make_binary_node lhs rhs (node_name top phi psi)
+    else if SSL.is_iff psi then
+      let lhs, rhs = SSL.get_iff_operands psi in
+        let lhs = create phi lhs in
+        let rhs = create phi rhs in
+        make_binary_node lhs rhs (node_name top phi psi)
     else match SSL.node_type psi with
     | Var _ -> make_leaf @@ SSL.show psi
     | Operator _ ->
-        make_leaf (node_name phi psi)
+        make_leaf (node_name top phi psi)
     | Connective [psi'] | Quantifier (_, psi') ->
         let tree' = create phi psi' in
-        make_unary_node tree' (node_name phi psi)
+        make_unary_node tree' (node_name top phi psi)
     | Connective [psi1; psi2] ->
         let lhs = create phi psi1 in
         let rhs = create phi psi2 in
-        make_binary_node lhs rhs (node_name phi psi)
+        make_binary_node lhs rhs (node_name top phi psi)
+    | Connective psis ->
+        let trees = List.map (create phi) psis in
+        mk_variadic_node trees (node_name top phi psi)
 
 end
-
-let id = ref (-1)
 
 module Dot = Graph.Graphviz.Dot
   (struct
     include G
 
-    let show_label = function `Left -> "L" | `Right -> "R"
+    let show_label = function `Left -> "L" | `Right -> "R" | `None -> ""
 
     let graph_attributes _ = [`Rankdir `TopToBottom]
     let default_vertex_attributes _ = []
@@ -115,8 +137,7 @@ module Dot = Graph.Graphviz.Dot
 end)
 
 let out_ast path phi =
-  (*let phi = RichSyntax.sugarize phi in*)
-  let g = fst (AST.create phi phi) in
+  let g = fst (AST.create ~top:true phi phi) in
   let channel = open_out path in
   Dot.output_graph channel g;
   close_out channel
