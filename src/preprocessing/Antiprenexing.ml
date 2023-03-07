2c95f157571c975585af9fdd35a57626c0687bce
Tomas Dacik <xdacik00@stud.fit.vutbr.cz>
2023-03-07 13:42:55 +0100

    Fix antiprenexing

--- src/preprocessing/Antiprenexing.ml
+++ src/preprocessing/Antiprenexing.ml
@@ -18,7 +18,7 @@ let rec push_star_aux xs psis = match xs with
     | plus, minus ->
       let phi_minus = push_star_aux xs minus in
       let phi_plus = push_star_aux xs plus in
-      SSL.mk_star [push @@ SSL.mk_exists [x] phi_plus; phi_minus]
+      SSL.mk_star [apply @@ SSL.mk_exists [x] phi_plus; phi_minus]
 
 and apply phi = match phi with
   | SSL.Var _ | SSL.Pure _ | SSL.Eq _ | SSL.Distinct _ -> phi
@@ -26,25 +26,25 @@ and apply phi = match phi with
 
   (* Forall over conjunction *)
   | SSL.Forall (xs, And (psi1, psi2)) ->
-    SSL.mk_and [push @@ SSL.mk_forall xs psi1; push @@ SSL.mk_forall xs psi2]
+    SSL.mk_and [apply @@ SSL.mk_forall xs psi1; apply @@ SSL.mk_forall xs psi2]
 
   (* Exists over disjunction *)
   | SSL.Exists (xs, Or (psi1, psi2)) ->
-    SSL.mk_or [push @@ SSL.mk_exists xs psi1; push @@ SSL.mk_exists xs psi2]
+    SSL.mk_or [apply @@ SSL.mk_exists xs psi1; apply @@ SSL.mk_exists xs psi2]
 
   (* Exists over star *)
   | SSL.Exists (xs, Star psis) -> push_star_aux xs psis
 
   (* Forall over star
   | SSL.Forall (xs, Star (psi1, psi2)) ->
-    SSL.mk_star [push @@ SSL.mk_forall xs psi1; push @@ SSL.mk_forall xs psi2]
+    SSL.mk_star [apply @@ SSL.mk_forall xs psi1; apply @@ SSL.mk_forall xs psi2]
   *)
 
-  | SSL.And (psi1, psi2) -> SSL.mk_and [push psi1; push psi2]
-  | SSL.Or (psi1, psi2) -> SSL.mk_or [push psi1; push psi2]
-  | SSL.Not psi -> SSL.mk_not (push psi)
-  | SSL.GuardedNeg (psi1, psi2) -> SSL.mk_gneg (push psi1) (push psi2)
-  | SSL.Star psis -> SSL.mk_star @@ List.map push psis
-  | SSL.Septraction (psi1, psi2) -> SSL.mk_septraction (push psi1) (push psi2)
-  | SSL.Forall (xs, psi) -> SSL.mk_forall xs (push psi)
-  | SSL.Exists (xs, psi) -> SSL.mk_exists xs (push psi)
+  | SSL.And (psi1, psi2) -> SSL.mk_and [apply psi1; apply psi2]
+  | SSL.Or (psi1, psi2) -> SSL.mk_or [apply psi1; apply psi2]
+  | SSL.Not psi -> SSL.mk_not (apply psi)
+  | SSL.GuardedNeg (psi1, psi2) -> SSL.mk_gneg (apply psi1) (apply psi2)
+  | SSL.Star psis -> SSL.mk_star @@ List.map apply psis
+  | SSL.Septraction (psi1, psi2) -> SSL.mk_septraction (apply psi1) (apply psi2)
+  | SSL.Forall (xs, psi) -> SSL.mk_forall xs (apply psi)
+  | SSL.Exists (xs, psi) -> SSL.mk_exists xs (apply psi)
