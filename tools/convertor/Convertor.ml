36ded97c00d405eb10182a40af2ec615b2684f34
Tomas Dacik <xdacik00@stud.fit.vutbr.cz>
2023-05-04 18:24:31 +0200

    Add Harrsh convertor

--- tools/convertor/Convertor.ml
+++ tools/convertor/Convertor.ml
@@ -10,6 +10,9 @@ let output_path = ref ""
 let _broom = ref false
 let broom () = !_broom
 
+let _imprecise = ref false
+let imprecise () = !_imprecise
+
 let input_fn filename = match filename with
   | "" -> failwith "No input file"
   | filename -> input := filename
@@ -21,6 +24,7 @@ let input_name path =
 let convertor = ref ""
 let get_convertor () = match !convertor with
   | "cvc5" -> (module CVC5Convertor : CONVERTOR)
+  | "harrsh" -> (module HarrshConvertor : CONVERTOR)
   | "grass" -> (module GrasshopperConvertor : CONVERTOR)
   | "sloth" -> (module SlothConvertor : CONVERTOR)
   | "songbird" | "sls" -> (module SongbirdConvertor : CONVERTOR)
@@ -32,6 +36,7 @@ let speclist = [
     ("--to", Arg.Set_string convertor,
      "Select output format (sloth | grass | songbird | slide)");
     ("--broom", Arg.Set _broom, "Broom preprocessing");
+    ("--pure-imprecise", Arg.Set _imprecise, "Input uses imprecise semantics of pure formulae.");
   ]
 
 let usage_msg = "astral-convertor <input> --to format [-o filename]"
@@ -40,7 +45,9 @@ let parse () = Arg.parse speclist input_fn usage_msg; !input
 
 let preprocess_variadic cond phi =
   if not cond
-  then RemoveVariadic.apply phi
+  then
+    let phi = RemoveVariadic.apply phi in
+    phi
   else phi
 
 let preprocess_precise cond phi =
@@ -48,27 +55,35 @@ let preprocess_precise cond phi =
   then PreciseToImprecise.to_imprecise phi
   else phi
 
+let preprocess_imprecise phi =
+  if imprecise ()
+  then
+    let p = PreciseToImprecise.to_precise phi in
+    p
+  else phi
+
+let preprocess_broom phi =
+  if broom () then
+    let g = SL_graph.compute phi in
+    BroomPreprocessing.apply g phi
+    |> PurePreprocessing.apply
+    |> SSL.normalise
+    |> Simplifier.simplify
+  else phi
+
 (** Entry point *)
 let () =
   let input_path = parse () in
   let module Convertor = (val get_convertor () : CONVERTOR) in
   let input = Parser.parse_file input_path in
-  let input =
-    if broom ()
-    then
-      let g = SL_graph.compute input.phi in
-      let phi =
-        BroomPreprocessing.apply g input.phi
-        |> PurePreprocessing.apply
-        |> SSL.normalise
-        |> Simplifier.simplify
-        |> SSL.normalise
-        |> preprocess_variadic Convertor.supports_variadic_operators
-        |> preprocess_precise Convertor.precise_semantics
-      in
-      {input with phi = phi}
-    else input
+  let phi =
+    SSL.normalise input.phi
+    |> preprocess_imprecise
+    |> preprocess_broom
+    |> preprocess_precise Convertor.precise_semantics
+    |> preprocess_variadic Convertor.supports_variadic_operators
   in
+  let input = {input with phi = phi} in
   match !output_path with
     | "" -> Format.printf "%s" (Convertor.convert input)
     | path -> Convertor.dump (path ^ "/" ^ input_name input_path) input
