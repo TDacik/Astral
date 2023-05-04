82f76bc8ca9a91fa4dcc55e5a9ebb00ae102999a
Tomas Dacik <xdacik00@stud.fit.vutbr.cz>
2023-05-04 18:20:26 +0200

    Preprocessor fixes

--- src/preprocessing/PreciseToImprecise.ml
+++ src/preprocessing/PreciseToImprecise.ml
@@ -2,6 +2,7 @@
  *
  * Author: Tomas Dacik (idacik@fit.vut.cz), 2023 *)
 
+open SSL
 
 (** Precise -> Imprecise *)
 
@@ -11,10 +12,16 @@ let to_imprecise_sh phi =
     | psi -> [psi]
   in
   let pure, spatial = List.partition SSL.is_pure (operands) in
-  SSL.mk_and [SSL.mk_and pure; SSL.mk_star spatial]
+  match pure, spatial with
+  | [], spatial -> SSL.mk_star spatial
+  | _ -> SSL.mk_and [SSL.mk_and pure; SSL.mk_star spatial]
 
 let to_imprecise_arbitrary phi =
-  SSL.map (fun psi -> if SSL.is_pure psi then SSL.mk_and [psi; SSL.mk_emp ()] else psi) phi
+  SSL.map (function
+    | Var x -> Var x
+    | psi when SSL.is_pure psi -> SSL.mk_and [psi; SSL.mk_emp ()]
+    | psi -> psi
+  ) phi
 
 let to_imprecise phi = match SSL.as_query phi with
   | QF_SymbolicHeap_SAT phi -> to_imprecise_sh phi
@@ -24,19 +31,45 @@ let to_imprecise phi = match SSL.as_query phi with
 (** Imprecise -> Precise *)
 
 let to_precise_sh phi =
-  SSL.map
-    (fun psi -> match psi with
-      | And (psi1, psi2) ->
-        if SSL.is_pure psi then SSL.mk_star [psi1; psi2]
-        else psi
-      | psi when SSL.is_pure psi -> SSL.mk_star [psi; SSL.mk_true ()]
-      | psi -> psi
-    ) phi
+  match phi with
+  | Eq _ | Distinct _ -> SSL.mk_star [phi; SSL.mk_true ()]
+  | phi ->
+    SSL.map
+      (fun psi -> match psi with
+        | Var x -> Var x
+        | And (psi1, psi2) -> SSL.mk_star [psi1; psi2]
+        | psi -> psi
+      ) phi
 
 let to_precise_arbitrary phi =
-  SSL.map (fun psi -> if SSL.is_pure psi then SSL.mk_star [psi; SSL.mk_true ()] else psi) phi
+  SSL.map (function
+    | Var x -> Var x
+    | psi when SSL.is_pure psi -> SSL.mk_star [psi; SSL.mk_true ()]
+    | psi -> psi
+ ) phi
 
-let to_precise phi = match SSL.as_query phi with
-  | QF_SymbolicHeap_SAT phi -> to_precise_sh phi
-  | QF_SymbolicHeap_ENTL (lhs, rhs) -> SSL.mk_gneg (to_precise_sh lhs) (to_precise_sh rhs)
+let rec is_pure_part = function
+  | Eq _ | Distinct _ -> true
+  | And (lhs, rhs) -> is_pure_part lhs && is_pure_part rhs
+  | _ -> false
+
+let rec is_spatial_part = function
+  | PointsTo _ | LS _ | DLS _ | NLS _ -> true
+  | Star psis -> List.for_all is_spatial_part psis
+  | _ -> false
+
+let is_imprecise_sh = function
+  | Eq _ | Distinct _ | PointsTo _ | LS _ | DLS _ | NLS _ -> true
+  | And (lhs, rhs) ->
+    (is_pure_part lhs && is_spatial_part rhs)
+    || (is_spatial_part lhs && is_pure_part rhs)
+  | _ -> false
+
+(* Translate formulae from SL with imprecise equalities. *)
+let to_precise phi = match phi with
+  | phi when is_imprecise_sh phi -> to_precise_sh phi
+  | GuardedNeg (lhs, rhs) when is_imprecise_sh lhs && is_imprecise_sh rhs ->
+    SSL.mk_gneg (to_precise_sh lhs) (to_precise_sh rhs)
+  | GuardedNeg (lhs, Exists (xs, rhs)) when is_imprecise_sh lhs && is_imprecise_sh rhs ->
+    SSL.mk_gneg (to_precise_sh lhs) (SSL.mk_exists xs @@ to_precise_sh rhs)
   | _ -> to_precise_arbitrary phi
