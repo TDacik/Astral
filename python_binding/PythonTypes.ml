51aa10674e81ec116131501cd5332166631f849f
Tomas Dacik <xdacik00@stud.fit.vutbr.cz>
2023-05-22 15:23:15 +0200

    Update python binding

--- python_binding/PythonTypes.ml
+++ python_binding/PythonTypes.ml
@@ -1,8 +1,14 @@
+(* Duplication of core Astral's types.
+ *
+ * This module duplicates python types in order to avoid python_lib dependencies in
+ * the solver. *)
+
+open Base
 open Python_lib
+open Python_lib.Defunc
 
 module Astral = Astral_lib
 
-(** Duplicated types *)
 
 module Sort = struct
 
@@ -17,15 +23,8 @@ module Sort = struct
     | Loc
     | Tupple of t list
     | Sum of t list
-  [@@deriving python]
-
-end
-
-module Variable = struct
-
-  type t = string * Sort.t [@@deriving python]
-
-  type smt_var = string * Sort.t [@@deriving python]
+    | Uninterpreted of string
+  [@@deriving compare, python]
 
 end
 
@@ -37,14 +36,21 @@ end
 
 module SMT = struct
 
+  module Variable = struct
+
+    type t = string * Sort.t [@@deriving python]
+
+  end
+
   type t = Astral.SMT.t =
     | Constant of string * Sort.t
-    | Variable of Variable.smt_var
+    | Variable of Variable.t
     | And of t list
     | Or of t list
     | Not of t
     | Implies of t * t
     | Iff of t * t
+    | IfThenElse of t * t * t
     | True
     | False
     | Equal of t list
@@ -72,7 +78,7 @@ module SMT = struct
     | Store of t * t * t
     | Membership of t * t
     | Subset of t * t
-    | Disjoint of t * t
+    | Disjoint of t list
     | Union of t list * Sort.t
     | Inter of t list * Sort.t
     | Diff of t * t
@@ -88,14 +94,25 @@ end
 
 module SSL = struct
 
+  module Variable = struct
+
+    type t = string * Sort.t [@@deriving compare, python]
+
+    let _type = Of_python.create ~type_name:"var" ~conv:t_of_python
+    let _list = Of_python.create ~type_name:"var list" ~conv:(list_of_python t_of_python)
+
+  end
+
   type t = Astral.SSL.t =
     | Var of Variable.t
     | Pure of SMT.t
+    | Emp
     | Eq of t list
     | Distinct of t list
     | PointsTo of t * t list
     | LS of t * t
     | DLS of t * t * t * t
+    | NLS of t * t * t
     | SkipList of int * t * t
     | And of t * t
     | Or of t * t
@@ -103,27 +120,22 @@ module SSL = struct
     | GuardedNeg of t * t
     | Exists of t list * t
     | Forall of t list * t
-    | Star of t * t
+    | Star of t list
     | Septraction of t * t
   [@@deriving python]
 
+  let _type = Of_python.create ~type_name:"formula" ~conv:t_of_python
+  let _list = Of_python.create ~type_name:"formula list" ~conv:(list_of_python t_of_python)
+
 end
 
-(*
+(* TODO: can we derive python of Map.t? *)
 module Model = struct
 
-  module Location = Int
-  module LocationTupple = struct type t = Location.t list end
-  module Stack = Map.Make(Variable)
-  module Heap = Map.Make(Location)
+  type stack = (SSL.Variable.t * int) list [@@deriving python]
 
-  type t = {
-    stack : Stack.t;
-    heap : Heap.t;
-    footprints : Footprint.t SSL.Map.t;
-    heaps : Heap.t SSL.Map.t;
+  type heap = (int * int list) list [@@deriving python]
 
-    [@@deriving python]
+  type t = stack * heap [@@deriving python]
 
 end
-*)
