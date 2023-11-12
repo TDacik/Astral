7cb7196ec4d83af134e8b253837f995a08af2203
Tomas Dacik <xdacik00@stud.fit.vutbr.cz>
2023-11-12 13:48:02 +0100

    Move set signature to separated file

---
+++ src/SMT/Set_sig.mli
@@ -0,0 +1,59 @@
+open SMT_sig
+
+module type SET = sig
+
+  include SMT_TERM with type t = SMT.t
+
+  val name : string
+  (** Name of the encoding used for logging. *)
+
+  (** Sorts *)
+
+  val mk_sort : Sort.t -> Sort.t
+
+  val get_elem_sort : t -> Sort.t
+
+  (** Variables *)
+
+  val mk_var : string -> Sort.t -> t
+
+  val mk_fresh_var : string -> Sort.t -> t
+
+  (** Construction of terms *)
+
+  val mk_empty : Sort.t -> t
+
+  val mk_singleton : t -> t
+
+  val mk_enumeration : Sort.t -> t list -> t
+
+
+  val mk_union : t list -> Sort.t -> t
+
+  val mk_inter : t list -> Sort.t -> t
+
+  val mk_diff : t -> t -> t
+
+  val mk_compl : t -> t
+
+  (** Predicates *)
+
+  val mk_mem : t -> t -> t
+
+  val mk_subset : t -> t -> t
+
+  val mk_disjoint : t -> t -> t
+
+  val mk_disjoint_list : t list -> t
+
+  val mk_eq_empty : t -> t
+
+  val mk_eq_singleton : t -> t -> t
+
+  (** Accessors and syntactic checks *)
+
+  val get_elems : t -> t list
+
+  val may_disjoint : t -> t -> bool
+
+end
