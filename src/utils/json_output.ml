dbc16c05b4dac83d3b70ef6b7c1024724710e836
Tomas Dacik <xdacik00@stud.fit.vutbr.cz>
2023-11-19 22:56:59 +0100

    [Utils] small changes

--- src/utils/json_output.ml
+++ src/utils/json_output.ml
@@ -4,25 +4,31 @@
 
 open Context
 
+module Options = Options_base
+
+let size context = match context.size with Some x -> string_of_int x | None -> "-"
+
+let solved_by context = match context.size with Some x -> "translation" | None -> "sl-graph"
+
 let input_to_json context =
   `Assoc [
     "Name",                 `String (Options.input_path ());
     "# variables",          `Int (List.length context.vars);
     "Expected status",      `String (Context.show_expected_status context);
     "Size",                 `Int (SSL.size context.phi);
-    "Location sort",        `String (Sort.show context.type_env.loc_sort);
-    "Heap sort",            `String (Sort.show context.type_env.heap_sort);
+    (*"Location sort",        `String (Sort.show context.type_env.loc_sort);*)
+    "Heap sort",            `String (Sort.show context.raw_input.heap_sort);
   ]
 
 let json_repr context =
   `Assoc [
     "Input",                input_to_json context;
-    "Formula size",         `Int (Option.get context.size);
+    "Solved by",            `String (solved_by context);
+    "Bounds",               Bounds.to_json context.bounds;
+    "Formula size",         `String (size context);
     "Status",               `String (Context.show_status context);
     "Model verified",       `String "-";
-    "Stack bound",          `String (Context.show_stack_bound context);
-    "Heap location bound",  `Int context.location_bound;
-    "Times",                Timer.json_repr ();
+    "Profiling",            Profiler.json_repr ();
     "Options",              Options.to_json ();
   ]
 
