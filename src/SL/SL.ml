633abe4eb8d819798a51b43bcd710c4ad4f4b3ef
TDacik <xdacik00@stud.fit.vutbr.cz>
2024-11-22 23:29:50 +0100

    Implement SL over BaseLogic, change SSL -> SL

---
+++ src/SL/SL.ml
@@ -0,0 +1,323 @@
+(* Implementation of separation logic over BaseLogic.
+ *
+ * Author: Tomas Dacik (xdacik00@fit.vutbr.cz), 2022 *)
+
+open MemoryModel
+
+module A = BaseLogic.Application
+module B = BaseLogic
+
+include BaseLogic
+
+module Variable = struct
+
+  include Variable
+
+  let is_pure var = not @@ Sort.is_loc @@ Variable.get_sort var
+
+end
+
+module Term = struct
+
+  include BaseLogic
+  include BaseLogic.SeparationLogic
+
+  let mk_smt = SMT.to_base_logic
+
+  type view =
+    | Var of Variable.t
+    | HeapTerm of Field.t * Sort.t * t
+    | SmtTerm of SMT.t
+    | BlockBegin of t
+    | BlockEnd of t
+
+  let view = function
+    | B.Variable var when Variable.is_loc var ->
+      Var (Variable.mk (Variable.show var) (Variable.get_sort var))
+    | B.Variable var -> SmtTerm (SMT.mk_var (Variable.show var) (Variable.get_sort var))
+    | B.Application (BlockBegin, [x]) -> BlockBegin x
+    | B.Application (BlockEnd, [x]) -> BlockEnd x
+    | B.Application (HeapTerm (field, sort), [x]) -> HeapTerm (field, sort, x)
+    | x -> SmtTerm (SMT.of_base_logic x) (* TODO: checks *)
+
+  let is_nil term = match view term with
+    | Var var -> Variable.is_nil var
+    | _ -> false
+
+  let is_heap_term t = match view t with HeapTerm _ -> true | _ -> false
+  let is_smt_term t = match view t with SmtTerm _ -> true | _ -> false
+
+  let get_subterm t = match view t with
+    | HeapTerm (_, _, t) | BlockBegin t | BlockEnd t -> t
+    | _ -> raise @@ Invalid_argument ("SL.Term.get_subterm: " ^ show t)
+
+end
+
+
+(** Separation Logic *)
+
+let to_smt : t -> SMT.t = SMT.of_base_logic
+let of_smt : SMT.t -> t = SMT.to_base_logic
+
+let of_term = Fun.id
+let to_term = Fun.id
+
+
+type view =
+  (* Atoms *)
+  | Emp
+  | True
+  | False
+  | Pure of SMT.t
+  | Eq of Term.t list
+  | Distinct of Term.t list
+  | PointsTo of Term.t * StructDef.t * Term.t list
+  | Predicate of string * Term.t list * StructDef.t list
+
+  (* Boolean connectives *)
+  | And of t list
+  | Or of t list
+  | Not of t
+  | GuardedNeg of t * t
+
+  (* Quantifiers *)
+  | Exists of Variable.t list * t
+  | Forall of Variable.t list * t
+
+  (* Spatial connectives *)
+  | Star of t list
+  | Septraction of t * t
+
+let view phi =
+  match phi with
+  | B.Variable var ->
+    (* Location variable always appear as SL.Term.Var
+    assert (not @@ Variable.is_loc var); *)
+    Pure (to_smt phi)
+  | B.Application (app, xs) -> begin match app with
+    | A.Emp -> Emp
+    | A.Constant (Bool true) -> True
+    | A.Constant (Bool false) -> False
+    | A.Equal -> Eq xs
+    | A.Distinct -> Distinct xs
+    | A.PointsTo s -> PointsTo (List.hd xs, s, List.tl xs)
+    | A.Predicate (p, defs) -> Predicate (Identifier.show p, xs, defs)
+    | A.And -> And xs
+    | A.GuardedNot -> GuardedNeg (List.nth xs 0, List.nth xs 1)
+    | A.Or -> Or xs
+    | A.Implies -> Or [B.Boolean.mk_not @@ List.nth xs 0; List.nth xs 1]
+    | A.Iff ->
+      let lhs = List.nth xs 0 in
+      let rhs = List.nth xs 1 in
+      And [B.Boolean.mk_implies lhs rhs; B.Boolean.mk_implies rhs lhs]
+    | A.Not -> Not (List.hd xs)
+    | A.Star -> Star xs
+    | A.Septraction -> Septraction (List.nth xs 0, List.nth xs 1)
+    | A.Pure -> Pure (to_smt @@ List.hd xs)
+
+    (** This case should happen only under Pure application *)
+    | _ -> Pure phi
+    end
+  | B.Binder (Exists None, vs, x) -> Exists (vs, x)
+  | B.Binder (Forall None, vs, x) -> Forall (vs, x)
+  |  _ -> Utils.internal_error ("Not an SL formula: " ^ show phi)
+
+let rec map_view fn phi =
+  let fn phi =
+    try fn phi
+    with Match_failure _ -> raise Exit
+  in
+  match phi with
+  | B.Variable _ -> begin
+    try fn @@ view phi
+    with Exit -> phi
+  end
+  | B.Application (app, psis) -> begin
+    let args = List.map (map_view fn) psis in
+    try fn @@ view @@ Application (app, args)
+    with Exit -> Application (app, args)
+  end
+  | B.Binder (binder, vs, psi) ->
+    let body = map_view fn psi in
+    try fn @@ view @@ Binder (binder, vs, body)
+    with Exit -> Binder (binder, vs, body)
+
+include BaseLogic.Boolean
+include BaseLogic.SeparationLogic
+
+(** Hide range parameters *)
+include BaseLogic.Quantifiers
+let mk_forall vars phi = BaseLogic.Quantifiers.mk_forall vars phi
+let mk_exists vars phi = BaseLogic.Quantifiers.mk_exists vars phi
+
+(** Shadow Boolean.mk_var *)
+let mk_var = BaseLogic.mk_var
+let mk_fresh_var = BaseLogic.mk_fresh_var
+
+(** Redefinition with compatible types *)
+let mk_pure smt = SeparationLogic.mk_pure (of_smt smt)
+
+
+let free_vars ?(with_nil=true) ?(with_pure=false) phi =
+  let vars = free_vars phi in
+  let vars =
+    if with_nil then vars
+    else BatList.remove_if Variable.is_nil vars
+  in
+  if not @@ with_pure then List.filter Variable.is_loc vars
+  else vars
+
+
+let is_atom phi = match view phi with
+  | Eq _ | Distinct _ | PointsTo _ | Predicate _ | Emp -> true
+  | _ -> false
+
+let is_pointer phi = match view phi with
+  | PointsTo _ -> true
+  | _ -> false
+
+let is_predicate phi = match view phi with
+  | Predicate _ -> true
+  | _ -> false
+
+let is_spatial_atom phi = match view phi with
+  | Predicate _ | PointsTo _ -> true
+  | _ -> false
+
+let is_atomic =
+  for_all_apps (function
+    | Predicate _ -> false
+    | _ -> true
+  )
+
+(* TODO: is emp pure or not? *)
+let is_pure =
+  for_all_apps (function
+    | Predicate _ | PointsTo _ | Emp | Star | Septraction -> false
+    | _ -> true
+   )
+
+let is_pure_smt phi =
+  is_pure phi && List.for_all (Variable.is_pure) (free_vars phi)
+
+let is_low_level = exists_app (function BlockBegin | BlockEnd -> true | _ -> false)
+
+(* TODO: will require normalisation for user-defined *)
+let get_root phi = match view phi with
+  | Predicate (_, x :: _, _) -> x
+
+let get_struct_def phi = match view phi with
+  | PointsTo (_, def, _) -> def
+
+let get_fields phi =
+  select_subformulae is_pointer phi
+  |> List.map get_struct_def
+  |> List.concat_map StructDef.get_fields
+
+let get_terms phi =
+  let subformulae = select_subformulae is_atom phi in
+  let get_terms_aux psi = match view psi with
+    | Emp -> []
+    | Distinct xs | Eq xs | Predicate (_, xs, _) -> xs
+    | PointsTo (x, _, ys) -> x :: ys
+  in
+  let terms = List.concat_map get_terms_aux subformulae in
+  BatList.unique ~eq:equal terms
+
+let get_loc_terms phi heap_sort =
+  let loc_sorts = HeapSort.get_loc_sorts heap_sort in
+  get_terms phi
+  |> List.filter (fun term -> BatList.mem_cmp Sort.compare (Term.get_sort term) loc_sorts)
+
+let get_terms_of_sort sort phi =
+  get_terms phi
+  |> List.filter (has_sort sort)
+
+let rec is_symbolic_heap phi = match phi with
+  | Variable _ -> true
+  | Application _ when is_atom phi -> true
+  | Application (Star, psis) -> List.for_all is_symbolic_heap psis
+  | Binder (Exists _, _, psi) -> is_symbolic_heap psi
+  | _ -> false
+
+let is_symbolic_heap_entl phi = match view phi with
+  | GuardedNeg (lhs, rhs) -> is_symbolic_heap lhs && is_symbolic_heap rhs
+  | _ -> false
+
+let is_positive =
+  for_all (function
+    | Application (Not, _) -> false
+    | _ -> true
+  )
+
+let is_negation_free =
+  for_all (function
+    | Application (Not, _) | Application (GuardedNot, _) -> false
+    | _ -> true
+  )
+
+let as_symbolic_heap phi = match view phi with
+  | Star psis -> List.partition is_pure psis
+  | _ when is_pure phi -> [phi], []
+  | _ -> [], [phi]
+
+let as_symbolic_heap' phi =
+  let pure, spatial = as_symbolic_heap phi in
+  pure @ spatial
+
+let as_entailment phi = match view phi with
+  | GuardedNeg (lhs, rhs) -> (lhs, rhs)
+  | _ -> failwith "Not an entailment"
+
+type fragment =
+  | SymbolicHeap_SAT
+  | SymbolicHeap_ENTL
+  | Positive
+  | Arbitrary
+  | Atomic
+
+let show_fragment = function
+  | SymbolicHeap_SAT -> "SH_SAT"
+  | SymbolicHeap_ENTL -> "SH_ENTL"
+  | Positive -> "positive"
+  | Atomic -> "atomic"
+  | Arbitrary -> "arbitrary"
+
+let classify_fragment phi =
+  if is_atomic phi then Atomic
+  else if is_symbolic_heap phi then SymbolicHeap_SAT
+  else if is_symbolic_heap_entl phi then SymbolicHeap_ENTL
+  else if is_positive phi then Positive
+  else Arbitrary
+
+type query =
+  | SymbolicHeap_SAT of t list
+  | SymbolicHeap_ENTL of t list * t list
+  | Arbitrary of t
+
+let as_query phi =
+  let open BatTuple in
+  if is_symbolic_heap phi then
+    SymbolicHeap_SAT (as_symbolic_heap' phi)
+  else if is_symbolic_heap_entl phi then
+    let lhs, rhs = Tuple2.mapn as_symbolic_heap' @@ as_entailment phi in
+    SymbolicHeap_ENTL (lhs, rhs)
+  else
+    Arbitrary phi
+
+module Infix = struct
+
+  let (==) x y = mk_eq [x; y]
+  let (!=) x y = mk_distinct [x; y]
+  let (|->) = mk_pto
+  let (|~>) = mk_ls
+
+  let (=>)  = mk_implies
+  let (<=>) x y = mk_iff [x; y]
+  let (&!)  = mk_gneg
+  let (&&) x y  = mk_and [x; y]
+  let (||) x y = mk_or [x; y]
+
+  let ( * ) x y = mk_star [x; y]
+
+end
---
+++ src/SL/SL.mli
@@ -0,0 +1,297 @@
+(* Representation of SL formulae.
+ *
+ * Author: Tomas Dacik (idacik@fit.vut.cz), 2021 *)
+
+open Logic_sig
+open Datatype_sig
+open MemoryModel
+
+type t
+
+module Variable : sig
+  include VARIABLE with module Sort = Sort
+
+  val nil : t
+
+  val is_nil : t -> bool
+
+  val is_loc : t -> bool
+
+  val is_pure : t -> bool
+
+end
+
+module Term : sig
+
+  type t
+
+  val nil : t
+
+  val is_nil : t -> bool
+
+  val is_heap_term : t -> bool
+
+  val is_smt_term : t -> bool
+
+  val mk_var : string -> Sort.t -> t
+
+  val mk_heap_term : Field.t -> Sort.t -> t -> t
+
+  val mk_smt : SMT.t -> t
+
+  val mk_block_begin : t -> t
+
+  val mk_block_end : t -> t
+
+  val of_var : Variable.t -> t
+
+  val get_subterm : t -> t
+  (** Assumes that the term is not a variable. *)
+
+  (** TODO: logic_sig? *)
+
+  include Logic_sig.SORTED with type t := t and module Sort = Sort
+
+  include Datatype_sig.COMPARABLE with type t := t
+  include Datatype_sig.PRINTABLE with type t := t
+  include Datatype_sig.COLLECTIONS with type t := t
+
+  val hash : t -> int
+
+  type view =
+    | Var of Variable.t
+    | HeapTerm of Field.t * Sort.t * t
+    | SmtTerm of SMT.t
+    | BlockBegin of t
+    | BlockEnd of t
+
+  val view : t -> view
+
+end
+
+(**/**)
+
+(** Some unsafe casting functions *)
+
+val to_smt : t -> SMT.t
+val of_term : Term.t -> t
+val to_term : t -> Term.t
+
+(**/**)
+
+
+include LOGIC
+  with type t := t
+   and type term := Term.t
+   and module Sort = Sort
+   and module Variable := Variable
+
+type view =
+  | Emp
+  | True
+  | False
+  | Pure of SMT.t
+  | Eq of Term.t list
+  | Distinct of Term.t list
+  | PointsTo of Term.t * StructDef.t * Term.t list
+  | Predicate of string * Term.t list * StructDef.t list
+  | And of t list
+  | Or of t list
+  | Not of t
+  | GuardedNeg of t * t
+  | Exists of Variable.t list * t
+  | Forall of Variable.t list * t
+  | Star of t list
+  | Septraction of t * t
+
+include Logic_sig.WITH_VIEW
+  with type t := t
+   and type view := view
+
+
+include COLLECTIONS with type t := t
+
+val of_smt : SMT.t -> t
+
+(** {2 Constructors} *)
+
+val nil : t
+
+val mk_var : string -> Sort.t -> t
+(** Create SL term consisting of a location variable with given name. *)
+
+val mk_fresh_var : string -> Sort.t -> t
+(** Create SL term consisting of a fresh location variable with given name. *)
+
+val mk_eq : Term.t list -> t
+
+val mk_eq2 : Term.t -> Term.t -> t
+
+val mk_distinct : Term.t list -> t
+
+val mk_distinct2 : Term.t -> Term.t -> t
+
+val mk_pto_struct : Term.t -> StructDef.t -> Term.t list -> t
+(** Low-level pointer constructor *)
+
+val mk_pto : Term.t -> Term.t -> t
+(** Create a term representing a pointer with a single target location. *)
+
+val mk_predicate : string -> ?structs:StructDef.t list -> Term.t list -> t
+(** Create an instance of an inductive predicate with given name. This instance can be
+    parametrised by a list of structures. For example, Astral's built-in list-segment
+    predicate can be parametrised by any structure with a single field with sort same
+    as the sort of instantance's arguments.*)
+
+val emp : t
+
+val tt : t
+
+val ff : t
+
+val mk_pure : SMT.t -> t
+(** Create a pure assertion as a separation logic formula. The input has to be of boolean
+    sort.
+
+    Note that pure formula are satisfiable only on empty footprints. Thus, (pure true) is
+    equivalent to emp. *)
+
+val mk_not : t -> t
+
+val mk_star : t list -> t
+
+val mk_and : t list -> t
+
+val mk_or : t list -> t
+
+val mk_implies : t -> t -> t
+
+val mk_iff : t list -> t
+
+val mk_ite : t -> t -> t -> t
+
+val mk_gneg : t -> t -> t
+
+val mk_septraction : t -> t -> t
+
+val mk_wand : t -> t -> t
+
+val mk_exists : Variable.t list -> t -> t
+
+val mk_forall : Variable.t list -> t -> t
+
+val mk_exists' : Sort.t list -> (Term.t list -> t) -> t
+
+val mk_forall' : Sort.t list -> (Term.t list -> t) -> t
+
+(** {2 Properties} *)
+
+val is_pointer : t -> bool
+
+val is_spatial_atom : t -> bool
+
+(** {2 Fragment classification} *)
+
+val is_pure : t -> bool
+(** Boolean combination of (dis-)equalities over location variables. *)
+
+val is_pure_smt : t -> bool
+(** No location variables. *)
+
+val is_atom : t -> bool
+
+val is_nil : t -> bool
+
+val is_predicate : t -> bool
+
+val is_atomic : t -> bool
+
+val is_symbolic_heap : t -> bool
+
+val is_symbolic_heap_entl : t -> bool
+
+val is_negation_free : t -> bool
+
+val is_positive : t -> bool
+
+val is_quantifier_free : t -> bool
+
+val is_low_level : t -> bool
+
+type fragment =
+  | SymbolicHeap_SAT
+  | SymbolicHeap_ENTL
+  | Positive
+  | Arbitrary
+  | Atomic
+
+val show_fragment : fragment -> string
+
+val classify_fragment : t -> fragment
+
+type query =
+  | SymbolicHeap_SAT of t list
+  | SymbolicHeap_ENTL of t list * t list
+  | Arbitrary of t
+
+val as_query : t -> query
+
+val as_entailment : t -> t * t
+
+val get_root : t -> Variable.t
+
+val get_fields : t -> Field.t list
+
+(** {2 Syntactic manipulation} *)
+
+val free_vars : ?with_nil:bool -> ?with_pure:bool -> t -> Variable.t list
+
+val free_vars_of_sort : Sort.t -> t -> Variable.t list
+
+val get_terms : t -> Term.t list
+
+val get_terms_of_sort : Sort.t -> t -> Term.t list
+
+val get_root : t -> Term.t
+
+(** {2 Operations requiring the sort of heap *)
+
+val get_loc_terms : t -> HeapSort.t -> Term.t list
+
+module Infix : sig
+
+  val (==) : Term.t -> Term.t -> t
+  (** Infix equality *)
+
+  val (!=) : Term.t -> Term.t -> t
+  (** Infix disequality *)
+
+  val (|->) : Term.t -> Term.t -> t
+  (** Infix pointer *)
+
+  val (|~>) : Term.t -> Term.t -> t
+  (** Infix singly-linked list *)
+
+  val (=>) : t -> t -> t
+  (** Infix implication *)
+
+  val (<=>) : t -> t -> t
+  (** Infix iff *)
+
+  val (&!) : t -> t -> t
+  (** Infix guarded negation *)
+
+  val (&&) : t -> t -> t
+  (** Infix conjucntion *)
+
+  val (||) : t -> t -> t
+  (** Infix disjunction *)
+
+  val ( * ) : t -> t -> t
+  (** Infix separating conjunction *)
+
+end
+
+(** {2 Misc} *)
+
+val as_symbolic_heap : t -> t list * t list
