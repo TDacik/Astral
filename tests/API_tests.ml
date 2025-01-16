a877ccdba81a0daafd08581bde264d2df6bac49b
Tomáš Dacík <xdacik00@stud.fit.vutbr.cz>
2025-01-16 19:01:17 +0100

    [Test] Add test for input dump

--- tests/API_tests.ml
+++ tests/API_tests.ml
@@ -18,10 +18,21 @@ let check_sat_test2 () =
   in
   assert (not @@ Solver.check_sat solver phi)
 
+
+let debug_input () =
+  let dirname = Format.asprintf "%s/_astral_test" (Filename.get_temp_dir_name ()) in
+  let solver = Solver.init ~dump_queries:(`Full dirname) () in
+  let phi = SL.emp in
+  let _ = Solver.check_sat solver phi in
+  assert (Sys.file_exists @@ dirname ^ "/query_0001/input.smt2")
+
 let () =
   run "API" [
+    "debug", [
+      test_case "input"   `Quick debug_input;
+    ];
     "check_sat", [
       test_case "sat"   `Quick check_sat_test1;
       test_case "unsat" `Quick check_sat_test2;
-    ]]
-
+    ]
+  ]
