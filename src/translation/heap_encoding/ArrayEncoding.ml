1c4d6637ac81490dfd667f7c7ceab56e56b7f39e
Tomáš Dacík <xdacik00@stud.fit.vutbr.cz>
2025-03-21 11:09:20 +0100

    [Encoding] fix heap axioms

--- src/translation/heap_encoding/ArrayEncoding.ml
+++ src/translation/heap_encoding/ArrayEncoding.ml
@@ -70,7 +70,7 @@ module Make (Locations : LOCATIONS) = struct
   let get_fields self : Field.t list = Field.Map.keys self.field_map
 
   (** Return arrays representing pointer fields *)
-  let get_pointer_arrays self : SMT.t list =
+  let get_linked_arrays self : SMT.t list =
     Field.Map.filter (fun f _ -> Field.is_pointer f) self.field_map
     |> Field.Map.values
 
@@ -171,7 +171,7 @@ module Make (Locations : LOCATIONS) = struct
 
   (* Axioms enforced by location encoding *)
   let heap_axioms self =
-    get_pointer_arrays self
+    get_linked_arrays self
     |> List.map (Locations.heap_axioms self.locs)
     |> Boolean.mk_and
 
