abb041953ed5300b6824956e0b41f4a5102f70d7
Tomáš Dacík <xdacik00@stud.fit.vutbr.cz>
2025-03-26 15:45:35 +0100

    [Bounds] Fix path bound computation

---
+++ benchmarks/97-slplugin/03.smt2
@@ -0,0 +1,15 @@
+(set-info :status unknown)
+
+(set-option :use-builtin-definitions)
+
+(declare-const A_s_5 Loc)
+(declare-const node_6!11 Loc)
+(declare-const node_6 Loc)
+(declare-const s_5 Loc)
+
+
+(assert
+ (sep (= node_6 s_5 A_s_5) (distinct s_5 nil) (pto node_6!11 (c_ls node_6))
+  (and (sep (ls s_5 node_6!11) (distinct s_5 node_6!11))
+   (not (pto s_5 (c_ls node_6!11))))
+  (ls node_6 s_5)))
--- src/bounds/PathBound.ml
+++ src/bounds/PathBound.ml
@@ -93,7 +93,7 @@ let alloc_bound g phi heap_sort field x y max =
         (*Sort.equal (SL.get_sort x) Sort.loc_nls (* TODO *)
         ||*) Sort.equal (SL.Term.get_sort x) (SL.Term.get_sort x')
       )
-  |> List.filter (fun x' -> not @@ SL.Term.equal x x')
+  |> List.filter (fun x' -> not @@ SL_graph.must_eq g x x')
   |> List.map (fun x -> root_weight g phi heap_sort x)
   |> BatList.sum
   |> (fun x -> max - x)
