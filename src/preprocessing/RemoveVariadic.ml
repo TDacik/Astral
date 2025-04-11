7df80c89597d7ed88b639b1a96da229a2eedf352
Tomáš Dacík <xdacik00@stud.fit.vutbr.cz>
2025-04-11 15:36:15 +0200

    [Preprocessing] Fix removal of variadic operators

--- src/preprocessing/RemoveVariadic.ml
+++ src/preprocessing/RemoveVariadic.ml
@@ -2,23 +2,35 @@
  *
  * Author: Tomas Dacik (idacik@fit.vut.cz), 2023 *)
 
-let rec aux combine fn = function
+let rec process_eq = function
+  | [x1; x2] -> SL.mk_eq [x1; x2]
+  | x1 :: x2 :: xs -> SL.mk_and [SL.mk_eq [x1; x2]; process_eq (x2 :: xs)]
+
+let process_distinct = function
+  | [x1; x2] -> SL.mk_distinct [x1; x2]
+  | xs ->
+    List_utils.diagonal_product xs
+    |> List.map (fun (x, y) -> SL.mk_distinct [x; y])
+    |> SL.mk_and
+
+let rec process_variadic fn = function
   | [x1; x2] -> fn [x1; x2]
-  | x1 :: x2 :: xs -> combine [fn [x1; x2]; aux combine fn (x2 :: xs)]
+  | x1 :: x2 :: xs -> fn [fn [x1; x2]; process_variadic fn xs]
 
 let rec apply phi =
   SL.map_view (fun psi -> match psi with
-    | SL.Eq xs -> aux SL.mk_and SL.mk_eq xs
-
-    | SL.Distinct [x1; x2] -> SL.mk_distinct [x1; x2]
+    | SL.Eq xs -> process_eq xs
+    | SL.Distinct xs -> process_distinct xs
+    (*
+      SL.mk_distinct [x1; x2]
     | SL.Distinct xs ->
       List_utils.diagonal_product xs
       |> List.map (fun (x, y) -> SL.mk_distinct [x; y])
       |> SL.mk_and
-
-    | SL.And psis -> aux SL.mk_and SL.mk_and psis
-    | SL.Or psis -> aux SL.mk_or SL.mk_or psis
-    | SL.Star psis -> aux SL.mk_star SL.mk_star psis
+    *)
+    | SL.And psis -> process_variadic SL.mk_and psis
+    | SL.Or psis -> process_variadic SL.mk_or psis
+    | SL.Star psis -> process_variadic SL.mk_star psis
 
     | SL.Exists ([x], psi) -> SL.mk_exists [x] psi
     | SL.Forall ([x], psi) -> SL.mk_forall [x] psi
--- tests/preprocessing/RemoveVariadic_tests.ml
+++ tests/preprocessing/RemoveVariadic_tests.ml
@@ -1,30 +1,35 @@
 module SL = SL_testable
 open SL
 
-let apply = RemoveVariadic.apply
+let check = SL.check_apply RemoveVariadic.apply
 
 let equal () =
-  let phi = SL.mk_eq [w; x; y; z] in
+  let input = SL.mk_eq [w; x; y; z] in
   let expected = (w == x) && (x == y && (y == z)) in
-  SL.check_equal (apply phi) expected
+  check ~input ~expected
 
 let distinct () =
-  let phi = SL.mk_distinct [x; y; z] in
+  let input = SL.mk_distinct [x; y; z] in
   let expected = (x != y) && (x != z && (y != z)) in
-  SL.check_equal (apply phi) expected
+  check ~input ~expected
 
-(*
-let exists () =
-  let phi = SL.mk_exists [x; y] (x == y) in
-  let expected = SL.mk_exists [x] @@ SL.mk_exists [y] (x == y) in
-  SL.check_equal (apply phi) expected
-*)
+let star_test1 () =
+  let input = SL.mk_star [x |-> nil; y |-> nil; z |-> nil] in
+  let expected = (x |-> nil) * ((y |-> nil) * (z |-> nil)) in
+  check ~input ~expected
+
+(*let exists () =
+ * let phi = SL.mk_exists [x; y] (x == y) in
+ * let expected = SL.mk_exists [x] @@ SL.mk_exists [y] (x == y) in
+ * SL.check_equal (apply phi) expected
+ *)
 
 let () =
   run "RemoveVariadic" [
     "apply", [
       test_case "equal" `Quick equal;
       test_case "distinct" `Quick distinct;
+      test_case "star" `Quick star_test1;
       (*test_case "exists" `Quick exists; *)
     ]
   ]
