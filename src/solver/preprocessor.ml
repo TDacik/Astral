c613e9c3e585667dc395aa5e7f82940bd4b1c391
Tomáš Dacík <xdacik00@stud.fit.vutbr.cz>
2025-10-14 10:42:08 +0200

    [Preprocessing] Compute location bounds after unfolding

--- src/solver/preprocessor.ml
+++ src/solver/preprocessor.ml
@@ -93,9 +93,6 @@ let second_phase_aux aggresive context =
     QuantifierElimination.apply_ctx, "quantifier_elim"
   ]
   in
-  let sl_graph = SL_graph.compute ctx2.phi in
-  let bounds = LocationBounds.compute ctx2.phi ctx2.raw_input.heap_sort sl_graph in
-  let ctx2 = {ctx2 with location_bounds = bounds} in (* TODO: take min? *)
 
   let ctx3 = apply_list ctx2 [
     GlobalSID.formula_preprocessing_ctx, "builtins";
@@ -103,7 +100,10 @@ let second_phase_aux aggresive context =
     QuantifierElimination.apply_ctx, "q_elim_2";
   ]
   in
-  remove_unused_elements ctx3, (Some bounds)
+  let sl_graph = SL_graph.compute ctx3.phi in
+  let bounds = LocationBounds.compute ctx3.phi ctx3.raw_input.heap_sort sl_graph in
+  let ctx = {ctx3 with location_bounds = bounds} in (* TODO: take min? *)
+  remove_unused_elements ctx, (Some bounds)
 
 let second_phase context = match Options_base.preprocessing () with
   | `None -> context, None
