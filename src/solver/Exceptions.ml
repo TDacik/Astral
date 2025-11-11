247bd3430316b3765495291943773e091b147a48
Tomáš Dacík <idacik@fit.vut.cz>
2025-11-11 14:47:43 +0100

    [Config] Remove old declarations

--- src/solver/Exceptions.ml
+++ src/solver/Exceptions.ml
@@ -6,8 +6,6 @@ exception UnknownResult of string * string
 
 exception UnsupportedFragment of string * string
 
-exception CmdOptionError of string * string * string
-
 exception InternalError of Printexc.raw_backtrace * string * string
 
 let unsupported_fragment ~reason ?(details="") =
@@ -19,9 +17,6 @@ let unknown_result ~reason ?(details="") =
 let internal_error ~reason ?(details="") =
   let trace = Printexc.get_callstack 1000 in
   raise @@ InternalError (trace, reason, details)
-
-let cmd_option_error ~param ~bad_value ~expected_values =
-  raise @@ CmdOptionError (param, bad_value, expected_values)
 
 (** Pretty printers *)
 let pretty_internal_error ?trace reason ~details =
