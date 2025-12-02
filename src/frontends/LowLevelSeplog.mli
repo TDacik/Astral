7b7930bade47770dfab0e18b3ab45e343f23f03f
Tomáš Dacík <idacik@fit.vut.cz>
2025-12-02 10:24:40 +0100

    [Frontends] Improve documentation of low-level SL.

--- src/frontends/LowLevelSeplog.mli
+++ src/frontends/LowLevelSeplog.mli
@@ -63,23 +63,31 @@ val tt : t
 (** The {i true} atom. *)
 
 val mk_eq : Term.t list -> t
-(** Equality of n terms. *)
+(** Equality of n terms.
+
+    @raises SortError when arguments do not have the same width. *)
 
 val mk_eq2 : Term.t -> Term.t -> t
-(** Binary equality terms. *)
+(** Binary equality terms.
+
+    @raises SortError when arguments do not have the same width. *)
 
 val mk_distinct : Term.t list -> t
-(** Pairwise disequality of n terms. *)
+(** Pairwise disequality of n terms.
+
+    @raises SortError when arguments do not have the same width. *)
 
 val mk_distinct2 : Term.t -> Term.t -> t
-(** Disequality of two terms. *)
+(** Disequality of two terms.
+
+    @raises SortError when arguments do not have the same width. *)
 
 val mk_pto : Term.t -> Term.t -> t
 (** [mk_pto x y] creates a poinst-to assertion {m x \mapsto y}. *)
 
 val mk_pto_array : ?const:Bitvector.t -> size:Term.t -> Term.t -> t
 (** [mk_pto x ~const ~size] creates an array poinst-to assertion {m x \mapsto const[size]}.
-    The default value of [const] is {m \bottom}. *)
+    The default value of [const] is {m \top}. *)
 
 val mk_star : t list -> t
 (** Separating conjunction. *)
