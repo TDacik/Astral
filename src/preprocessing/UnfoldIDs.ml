8a88f4da7a1276ca2e1c621b8c1f2605f999346a
Tomáš Dacík <idacik@fit.vut.cz>
2026-01-23 22:35:38 +0100

    [Translation] Unfolding

--- src/preprocessing/UnfoldIDs.ml
+++ src/preprocessing/UnfoldIDs.ml
@@ -67,9 +67,10 @@ let must_allocate_lhs phi heap_sort lhs g name =
 
 let unfold_predicate_lhs sid phi lhs name xs =
   let bound = GlobalSID.unfolding_depth name in
+  Logger.debug "Unfolding predicate %s(%s): %d\n" name (SL.Term.show_list xs) bound;
   SID.unfold sid name xs bound
 
-let unfold_lhs sid g heap_sort bound phi lhs rhs = SL.map_view (function
+let unfold_lhs sid g heap_sort phi lhs rhs = SL.map_view (function
   | Predicate (name, xs, _) when SID.is_user_defined sid name ->
     `Modify (unfold_predicate_lhs sid phi lhs name xs)
     (*
@@ -97,25 +98,25 @@ let unfold_rhs sid ctx sl_graph lhs rhs =
   let module Translation = Translation.Make(Encoding)(Backend) in
   let module Unfolder = IncrementalUnfolding.Make(Encoding)(IncrementalBackend) in
 
-  (* TODO: avoid duplicated computation *)
-  let open Context in
-  let bounds = LocationBounds.compute lhs ctx.raw_input.heap_sort sl_graph in
 
   let lhs_t = Translation.translate {ctx with phi = lhs} in (* TODO: check*)
-  Unfolder.unfold ctx lhs_t rhs
+  Unfolder.unfold ctx lhs lhs_t rhs
 
 let unfold_default ctx phi =
   let open Context in
   let sid = GlobalSID.get () in
-  let location_bound = ctx.location_bounds in
   match SL.view phi with
     | _ when SL.is_symbolic_heap phi ->
       {ctx with phi = unfold_sat sid phi}
     | GuardedNeg (lhs, rhs) ->
       (* Here we assume that quantifier elimination for LHS was already performed. *)
       let sl_graph = SL_graph.compute lhs in
-      let ctx_lhs = {ctx with phi = Simplifier.simplify @@ unfold_lhs sid sl_graph ctx.heap_sort location_bound phi lhs rhs} in
+      let ctx_lhs = {ctx with phi = Simplifier.simplify @@ unfold_lhs sid sl_graph ctx.heap_sort phi lhs rhs} in
       let lhs = ctx_lhs.phi in
+      (* TODO: avoid duplicated computation *)
+        let open Context in
+        let bounds = LocationBounds.compute lhs ctx.raw_input.heap_sort sl_graph in
+        let ctx = {ctx with location_bounds = bounds} in
       let rhs = unfold_rhs sid ctx sl_graph lhs rhs in
       {ctx with phi = SL.mk_gneg lhs rhs; model_adapter = ctx_lhs.model_adapter}
     | False | True -> ctx
