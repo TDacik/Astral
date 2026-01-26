9c2c9e70ad685c073bde9efe73c4490683092352
Tomáš Dacík <idacik@fit.vut.cz>
2026-01-26 12:05:40 +0100

    [Bounds] Fix bound computation for non-entailments

--- src/bounds/LocationBounds.ml
+++ src/bounds/LocationBounds.ml
@@ -160,7 +160,9 @@ let rec compute_atomic psi =
   let poly = List.length @@ SL.Term.MonoList.unique @@ SL.get_terms_of_sort Sort.loc_nil psi in
   LocationBounds0.add Sort.loc_nil (SortBound.init 0 poly) res
 
-let compute phi heap_sort g =
-  (*| SymbolicHeap_ENTL (lhs, rhs) -> compute_sh_entl phi lhs rhs heap_sort g*)
-  if SL.is_atomic phi then compute_atomic phi
-  else compute_general phi heap_sort g
+let compute phi heap_sort g = match SL.classify_fragment phi with
+  | SymbolicHeap_ENTL ->
+    let lhs, rhs = SL.as_entailment phi in
+    if SL.is_atomic lhs then compute_atomic lhs
+    else compute_general phi heap_sort g
+  | _ -> compute_general phi heap_sort g
