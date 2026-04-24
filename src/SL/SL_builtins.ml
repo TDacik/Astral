6de0195c23d76955e5eb3baad4c5fdab5949455e
Tomáš Dacík <idacik@fit.vut.cz>
2026-04-24 12:57:05 +0200

    [SL] Add builtin structures

--- src/SL/SL_builtins.ml
+++ src/SL/SL_builtins.ml
@@ -6,6 +6,10 @@ let loc_ls = LS.loc_ls
 let loc_dls = DLS.loc_dls
 let loc_nls = NLS.loc_nls
 
+let struct_ls = LS.struct_ls
+let struct_dls = DLS.struct_dls
+let struct_nls = NLS.struct_nls
+
 let mk_pto_ls = LS.mk_pto
 let mk_pto_dls = DLS.mk_pto
 let mk_pto_nls = NLS.mk_pto
