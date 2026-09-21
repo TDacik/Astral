2d762dacde080e2ebc3a345a20899168d0d61714
Tomáš Dacík <idacik@fit.vut.cz>
2026-09-21 13:48:54 +0200

    Add preprocessing pass for heap-term elimination

---
+++ src/preprocessing/HeapTermElimination.ml
@@ -0,0 +1,15 @@
+(* Elimination of heap terms.
+ *
+ * Author: Tomas Dacik (idacik@fit.vut.cz), 2026 *)
+
+let rec elim_heap_term phi term = match SL.Term.view term with
+  | HeapTerm (field, base) ->
+    let base' = elim_heap_term phi base in
+    let target = SL.find_pto_target phi base' field in
+    Option.value ~default:term target
+  | Var _ -> term
+  | _ -> failwith "TODO"
+
+let apply phi =
+  assert (SL.is_symbolic_heap phi);
+  SL.map_terms (elim_heap_term phi) phi
